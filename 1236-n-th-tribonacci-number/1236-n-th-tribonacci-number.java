de2b376a2177ded077469fb2eb347a5426f7f23b
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2025-09-12 10:32:50 +0530

    Time: 0 ms (100%), Space: 40.4 MB (65.31%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.java
@@ -4,10 +4,7 @@ class Solution {
         if (n == 1) return 1;
         if(n==2) return 1;
 
-        int T0=0;
-        int T1=1;
-        int T2=1;
-        int T3=0;
+        int T0=0,T1=1,T2=1,T3=0;
         
         for(int i=3; i<=n; i++){
             T3=T0+T1+T2;
