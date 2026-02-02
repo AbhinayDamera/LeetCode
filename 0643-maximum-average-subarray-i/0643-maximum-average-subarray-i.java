7c4952d67071b89bda19a8e32f4ec9b7a5dfdf4c
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2026-02-02 20:25:05 +0530

    Time: 3 ms (84.58%), Space: 69.7 MB (37.83%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,66 +1,18 @@
-// class Solution {
-//     public double findMaxAverage(int[] nums, int k) {
-//         double maxAvg=-10000;
-//         for(int i=0; i<=nums.length-k; i++){
-//             int sum=0;
-
-//             for(int j=1;j<i+k;j++){
-//                 sum+=nums[j];
-//             }
-//             double avg=sum/k;
-
-//             if(avg>maxAvg){
-//                 maxAvg=avg;
-//             }
-//         }
-//         return maxAvg;
-//     }
-// }
-
-// class Solution {
-//     public double findMaxAverage(int[] nums, int k) {
-//         double maxAvg = -10000; // Set to a very small number
-
-//         for (int i = 0; i <= nums.length - k; i++) {
-//             int sum = 0;
-
-//             // Add k elements starting from index i
-//             for (int j = i; j < i + k; j++) {
-//                 sum += nums[j];
-//             }
-
-//             double avg = (double) sum / k;
-
-//             // Update maxAvg if a bigger average is found
-//             if (avg > maxAvg) {
-//                 maxAvg = avg;
-//             }
-//         }
-
-//         return maxAvg;
-//     }
-// }
-
-
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-    
-    int left=0;
-    int curSum=0;
-    int maxSum=0;
+        int n=nums.length;
+        int sum=0;
+        for(int i=0; i<k;i++){
+            sum+=nums[i];
+        }
+        int maxSum=sum;
+        for(int i=k; i<n; i++){
+            sum -= nums[i - k];  //removeleftmostelement
+            sum += nums[i];      // add new element
 
-    for(int i=0; i<k;i++){
-        curSum+=nums[i];
+            maxSum=Math.max(maxSum,sum);
+        }
+        return (double) maxSum/k;
+
     }
-    maxSum=curSum;
-    for(int right=k; right<nums.length;right++){
-        curSum-=nums[left];
-        left++;
-        curSum+=nums[right];
-        //right++;
-        maxSum=Math.max(maxSum,curSum);
-    }
-    return (double)maxSum/k;
-    }
-    
 }
