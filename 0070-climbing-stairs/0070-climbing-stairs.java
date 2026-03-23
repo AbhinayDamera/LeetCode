dcbf5ebd936bdc7e95bdacbf267679fa902ee0cb
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2026-03-23 19:58:11 +0530

    Time: 0 ms (100%), Space: 42.2 MB (48.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,27 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         int[] ab = new int[n+1];
+//         if(n<2){
+//             return n;
+//         }
+//         //ab[0]=0;
+//         ab[1]=1;
+//         ab[2]=2;
+//         for(int i=3;i<=n;i++){
+//             ab[i]=ab[i-1]+ab[i-2];
+//         }
+//         return ab[n];
+//     }
+// }
 class Solution {
     public int climbStairs(int n) {
-        int[] ab = new int[n+1];
+        int [] ab=new int[n+1];
         if(n<2){
             return n;
         }
-        //ab[0]=0;
         ab[1]=1;
         ab[2]=2;
-        for(int i=3;i<=n;i++){
+        for(int i=3; i<=n; i++){
             ab[i]=ab[i-1]+ab[i-2];
         }
         return ab[n];
