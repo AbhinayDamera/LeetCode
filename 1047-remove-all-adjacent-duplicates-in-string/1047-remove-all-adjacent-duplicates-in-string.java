2b24f6ebefcffa4d24c69e5a8107d9dbdb9fe927
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2026-05-06 11:18:40 +0530

    Time: 30 ms (40.12%), Space: 47.1 MB (65.64%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,44 +1,49 @@
-// class Solution {
-//     public String removeDuplicates(String s) {
-//         Stack<Character> stack = new Stack<>();
-//         for (int i = 0; i < s.length(); i++) {
-//             char ch=s.charAt(i);
-
-//             if(!stack.isEmpty() && stack.peek()==ch){
-//                 stack.pop();
-//             }else{
-//                 stack.push(ch);
-//             }
-//         }
-//         String result="";
-//         for(int i=0; i<stack.size();i++){
-//             result=result+stack.get(i);
-//         }
-//         return result;
-
-//     }
-// }
-
-
 class Solution {
     public String removeDuplicates(String s) {
 
-        Stack<Character>a1=new Stack<>();
-        for(int i=0; i<s.length();i++){
-            char ch=s.charAt(i);
+        Stack<Character> a1 = new Stack<>();
 
-            if(!a1.isEmpty() && a1.peek()==ch){
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(!a1.isEmpty() && a1.peek() == ch){
                 a1.pop();
             }
             else{
-                a1.add(ch);
+                a1.push(ch);
             }
         }
-        String results="";
-        for(int i=0; i<a1.size(); i++){
-            results=results+a1.get(i);
-        }
-        return results;
-    }
 
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < a1.size(); i++){
+            result.append(a1.get(i));
+        }
+
+        return result.toString();
+    }
 }
+
+
+// class Solution {
+//     public String removeDuplicates(String s) {
+
+//         Stack<Character>a1=new Stack<>();
+//         for(int i=0; i<s.length();i++){
+//             char ch=s.charAt(i);
+
+//             if(!a1.isEmpty() && a1.peek()==ch){
+//                 a1.pop();
+//             }
+//             else{
+//                 a1.add(ch);
+//             }
+//         }
+//         String results="";
+//         for(int i=0; i<a1.size(); i++){
+//             results=results+a1.get(i);
+//         }
+//         return results;
+//     }
+
+// }
