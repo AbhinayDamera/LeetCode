14502fa86cdf8cfc16b1ee27997ca0333674f75f
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2026-05-06 11:18:33 +0530

    Time: 123 ms (10.79%), Space: 49 MB (7.7%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,20 +1,44 @@
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         Stack<Character> stack = new Stack<>();
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch=s.charAt(i);
+
+//             if(!stack.isEmpty() && stack.peek()==ch){
+//                 stack.pop();
+//             }else{
+//                 stack.push(ch);
+//             }
+//         }
+//         String result="";
+//         for(int i=0; i<stack.size();i++){
+//             result=result+stack.get(i);
+//         }
+//         return result;
+
+//     }
+// }
+
+
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
+
+        Stack<Character>a1=new Stack<>();
+        for(int i=0; i<s.length();i++){
             char ch=s.charAt(i);
 
-            if(!stack.isEmpty() && stack.peek()==ch){
-                stack.pop();
-            }else{
-                stack.push(ch);
+            if(!a1.isEmpty() && a1.peek()==ch){
+                a1.pop();
+            }
+            else{
+                a1.add(ch);
             }
         }
-        String result="";
-        for(int i=0; i<stack.size();i++){
-            result=result+stack.get(i);
+        String results="";
+        for(int i=0; i<a1.size(); i++){
+            results=results+a1.get(i);
         }
-        return result;
-
+        return results;
     }
+
 }
