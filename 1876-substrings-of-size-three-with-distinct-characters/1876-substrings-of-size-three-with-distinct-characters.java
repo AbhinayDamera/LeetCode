5ada94c8a6997aad449a664bb8f83146cddd550d
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2026-06-12 12:11:56 +0530

    Time: 1 ms (96.37%), Space: 42.7 MB (79.66%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -13,3 +13,46 @@ class Solution {
         return count;
     }
 }
+
+// class Solution {
+//     public int countGoodSubstrings(String s) {
+//         int n = s.length();
+//         if (n < 3) return 0;
+
+//         int[] freq = new int[26];
+//         int count = 0;
+
+//         // first window of size 3
+//         for (int i = 0; i < 3; i++) {
+//             freq[s.charAt(i) - 'a']++;
+//         }
+
+//         if (isDistinct(freq)) count++;
+
+//         // slide the window
+//         for (int i = 3; i < n; i++) {
+//             // add new character (right side)
+//             freq[s.charAt(i) - 'a']++;
+
+//             // remove old character (left side)
+//             freq[s.charAt(i - 3) - 'a']--;
+
+//             if (isDistinct(freq)) count++;
+//         }
+
+//         return count;
+//     }
+
+//     private boolean isDistinct(int[] freq) {
+//         int unique = 0;
+//         for (int i = 0; i < freq.length; i++) {
+//             if (freq[i] > 0) {
+//                 unique++;
+//             }
+//         }
+//         if (unique == 3){
+//             return true;
+//         }
+//         return false;   
+//     }
+// }
