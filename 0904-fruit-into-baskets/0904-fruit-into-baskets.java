596bcc4831ebc8de86c84cd94313907f75578d4c
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2026-08-30 11:41:28 +0530

    Time: 60 ms (10.36%), Space: 70.5 MB (42.48%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer, Integer> basket = new HashMap<>();
+        int left = 0;
+        int maxFruits = 0;
+
+        for (int right = 0; right < fruits.length; right++) {
+
+            // Add fruit to basket
+            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
+
+            // Shrink window if more than 2 types
+            while (basket.size() > 2) {
+                basket.put(fruits[left], basket.get(fruits[left]) - 1);
+
+                if (basket.get(fruits[left]) == 0) {
+                    basket.remove(fruits[left]);
+                }
+                left++; // always move left
+            }
+
+            // Update max window size
+            maxFruits = Math.max(maxFruits, right - left + 1);
+        }
+
+        return maxFruits;
+    }
+}
