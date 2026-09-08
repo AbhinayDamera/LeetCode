6d595cd426d5f38e04a420b9af3de8792d879999
Abhinay damera <157620688+AbhinayDamera@users.noreply.github.com>
2026-09-08 12:36:11 +0530

    Time: 1 ms (99.48%), Space: 46.1 MB (50.44%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -1,16 +1,40 @@
+// class Solution {
+//     public int[] getSneakyNumbers(int[] nums) {
+//         int n=nums.length;
+//         int res[]=new int[2];
+//         int idx=0;
+//         Arrays.sort(nums);
+
+//         for(int i=1; i<n; i++){
+//             if(nums[i]==nums[i-1]){
+//                 res[idx]=nums[i];
+//                 idx++;
+//             }
+//         }
+//         return res;
+//     }
+// }
+
+
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
-        int n=nums.length;
-        int res[]=new int[2];
-        int idx=0;
-        Arrays.sort(nums);
 
-        for(int i=1; i<nums.length; i++){
-            if(nums[i]==nums[i-1]){
-                res[idx]=nums[i];
+        int[] freq = new int[101];   // assuming values range 0–100
+        int[] res = new int[2];
+        int idx = 0;
+
+        // expanded for-each loop
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            freq[num]++;
+
+            if (freq[num] == 2) {   // duplicate found
+                res[idx] = num;
                 idx++;
             }
         }
+
         return res;
     }
 }
